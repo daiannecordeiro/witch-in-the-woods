df2081d92c80b450d4a7d54819b72d934fcf272a
Daianne Cordeiro <daianne.nc@gmail.com>
2022-06-23 16:12:52 -0300

    adiciona clique para jogar em touchscreen

--- index.js
+++ index.js
@@ -51,3 +51,37 @@ const loopGame = setInterval(() => {
 
         clearInterval(loopGame);
     }},10)
+
+    document.addEventListener("click", pulo)
+
+const loopGameTouch = setInterval(() => {
+
+    const pumpkinPosition = pumpkin.offsetLeft;
+    const witchPosition = +window.getComputedStyle(witch).bottom.replace("px", "");
+
+    
+
+    if (pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) {
+        pumpkin.style.animation = "none";
+        pumpkin.style.left = `${pumpkinPosition}px`
+
+        witch.style.animation = "none";
+        witch.style.bottom = `${witchPosition}px`;
+        
+        witch.src = "assets/img/Witch/witch-dead.gif";
+        witch.style.width = "358px";
+        witch.style.height = "490px";
+        witch.style.marginLeft = "45px";
+
+        layer0.style.animation = "none";
+        layer1.style.animation = "none";
+        layer2.style.animation = "none";
+        layer3.style.animation = "none";
+        layer4.style.animation = "none";
+        layer5.style.animation = "none";
+        layer6.style.animation = "none";
+
+        gameOver.style.display = "block";
+
+        clearInterval(loopGame);
+    }},10)
