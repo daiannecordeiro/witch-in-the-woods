5c0508e9c3184d1c87f574a21e772d98d08081b1
Daianne Cordeiro <daianne.nc@gmail.com>
2022-06-23 22:43:47 -0300

    adiciona inimigo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const witch = document.querySelector('.witch');
 const pumpkin = document.querySelector('.pumpkin');
+const thorn = document.querySelector('.thorn');
 const gameOver = document.querySelector('.game-over');
 const score = document.querySelector('.score');
 
@@ -22,51 +23,21 @@ const pulo = () => {
 }
 
 document.addEventListener("keydown", pulo)
+document.addEventListener("click", pulo)
 
 const loopGame = setInterval(() => {
 
     const pumpkinPosition = pumpkin.offsetLeft;
+    const thornPosition = thorn.offsetLeft;
     const witchPosition = +window.getComputedStyle(witch).bottom.replace("px", "");
-
     
 
-    if (pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) {
+    if ((pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) || (thornPosition <= 180 && thornPosition > 0 && witchPosition < 80 )) {
         pumpkin.style.animation = "none";
-        pumpkin.style.left = `${pumpkinPosition}px`
+        pumpkin.style.left = `${pumpkinPosition}px`;
 
-        witch.style.animation = "none";
-        witch.style.bottom = `${witchPosition}px`;
-        
-        witch.src = "assets/img/Witch/witch-dead.gif";
-        witch.style.width = "358px";
-        witch.style.height = "490px";
-        witch.style.marginLeft = "45px";
-
-        layer0.style.animation = "none";
-        layer1.style.animation = "none";
-        layer2.style.animation = "none";
-        layer3.style.animation = "none";
-        layer4.style.animation = "none";
-        layer5.style.animation = "none";
-        layer6.style.animation = "none";
-
-        gameOver.style.display = "block";
-
-        clearInterval(loopGame);
-    }},10)
-
-    document.addEventListener("click", pulo)
-
-const loopGameTouch = setInterval(() => {
-
-    const pumpkinPosition = pumpkin.offsetLeft;
-    const witchPosition = +window.getComputedStyle(witch).bottom.replace("px", "");
-
-    
-
-    if (pumpkinPosition <= 120 && pumpkinPosition > 0 && witchPosition < 80) {
-        pumpkin.style.animation = "none";
-        pumpkin.style.left = `${pumpkinPosition}px`
+        thorn.style.animation = "none";
+        thorn.style.left = `${thornPosition}px`;
 
         witch.style.animation = "none";
         witch.style.bottom = `${witchPosition}px`;
